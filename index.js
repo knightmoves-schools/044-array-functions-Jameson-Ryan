0dde45e885c9df2fe082ae091d0554040de2a17d
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-10 14:01:00 -0600

    Modify automobiles array with additions and removals
    Added various vehicles to the automobiles array and manipulated it.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let automobiles = ['car','truck','motorcycle'];
 
+automobiles.push('airplane');
+automobiles.push('skateboard');
+automobiles.unshift('bike');
+automobiles.unshift('helicopter');
+
+let automobilesCopy = `${automobiles}`;
+
+automobiles.pop();
+automobiles.shift();
